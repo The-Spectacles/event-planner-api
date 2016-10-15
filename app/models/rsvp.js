f0d889d4ee7da580e6f4a29e314fa5bf76d092ce
styletab <katherinetab@gmail.com>
2016-10-15 13:54:40 -0400

    Create rsvp model
    Created rsvp model to match events model.

---
+++ app/models/rsvp.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const rsvpSchema = new mongoose.Schema({
+  _owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  _event: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Event',
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  startTime: {
+    type: Date,
+    required: false,
+  },
+  endTime: {
+    type: Date,
+    required: false,
+  },
+  questions: {
+    type: Array,
+    default: [
+      {
+        text: 'Are you coming?',
+        options: ['Yes', 'No', 'Maybe'],
+      },
+    ],
+  },
+}, {
+  timestamps: true,
+  toJSON: { virtuals: true },
+});
+
+
+const Rsvp = mongoose.model('Rsvp', rsvpSchema);
+
+module.exports = Rsvp;
