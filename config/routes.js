091b28d5d2d43205d7030f295aa2b1a10a8d5e48
rebekahheacock <rebekah.heacock@gmail.com>
2016-10-15 14:52:22 -0400

    Set up routes for events and rsvps
    Create all resource routes for events and rsvps

--- config/routes.js
+++ config/routes.js
@@ -9,6 +9,8 @@ module.exports = require('lib/wiring/routes')
 
 // standards RESTful routes
 .resources('examples')
+.resources('events')
+.resources('rsvps')
 
 // users of the app have special requirements
 .post('/sign-up', 'users#signup')
